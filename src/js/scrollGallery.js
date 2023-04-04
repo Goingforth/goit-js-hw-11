e72be0a364fa8a07a58e7e1a2a1606c9d7a47cb8
Oleksandr Siukov <as-service2008@ukr.net>
2023-04-04 12:51:51 +0300

    Update scrollGallery.js

--- src/js/scrollGallery.js
+++ src/js/scrollGallery.js
@@ -5,7 +5,8 @@ export default function scrollGallery(counter) {
     const { height: cardHeight } =
       gallery.firstElementChild.getBoundingClientRect();
     window.scrollBy({
-      top: cardHeight * 2 - 60,
+      //top: cardHeight * 2 - 60,
+      top: cardHeight * 2,
       behavior: 'smooth',
     });
   }
